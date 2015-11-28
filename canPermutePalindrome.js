d1c925b3df1c1b918c99338e437c6a247aebeaf5
Raymond Ho <chunkiat82@gmail.com>
2015-11-28 09:34:11 +0800

    adding problem statement

--- canPermutePalindrome.js
+++ canPermutePalindrome.js
@@ -1,3 +1,9 @@
+// Given a string, determine if a permutation of the string could form a palindrome.
+
+// For example,
+
+// "code" -> False, "aab" -> True, "carerac" -> True.
+
 /**
  * @param {string} s
  * @return {boolean}
