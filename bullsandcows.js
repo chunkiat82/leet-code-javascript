26da912159d921b2e1079b53b9ab4eb97af629ab
Raymond Ho <chunkiat82@gmail.com>
2015-12-02 09:19:15 +0800

    new problem but solution sucks

---
+++ bullsandcows.js
@@ -0,0 +1,55 @@
+// You are playing the following Bulls and Cows game with your friend: You write down a number and ask your friend to guess what the number is. Each time your friend makes a guess, you provide a hint that indicates how many digits in said guess match your secret number exactly in both digit and position (called "bulls") and how many digits match the secret number but locate in the wrong position (called "cows"). Your friend will use successive guesses and hints to eventually derive the secret number.
+
+// For example:
+
+// Secret number:  "1807"
+// Friends guess: "7810"
+// Hint: 1 bull and 3 cows. (The bull is 8, the cows are 0, 1 and 7.)
+// Write a function to return a hint according to the secret number and friend's guess, use A to indicate the bulls and B to indicate the cows. In the above example, your function should return "1A3B".
+
+// Please note that both secret number and friend's guess may contain duplicate digits, for example:
+
+// Secret number:  "1123"
+// Friend's guess: "0111"
+// In this case, the 1st 1 in friend's guess is a bull, the 2nd or 3rd 1 is a cow, and your function should return "1A1B".
+// You may assume that the secret number and your friend's guess only contain digits, and their lengths are always equal.
+
+// loop through all guess and find secret
+// if there is a duplicate we keep the count;
+
+//not good!
+const getHint = function(secret, guess) {
+
+    const sArr = secret.split('');
+    const gArr = guess.split('');
+
+    const bulls = [];
+    const cows = [];
+    const temp = [];
+
+    gArr.forEach((g,gI)=>{
+        sArr.forEach((s,sI)=>{
+            if (g===s){
+                if (gI===sI){
+                    if (bulls.indexOf(g) < 0){
+                        bulls[bulls.length] = g;
+                    }    
+                } else {
+                    if (cows.indexOf(g) < 0){
+                        cows[cows.length] = g;
+                    }
+                }                
+            }
+        });
+    });
+
+    // console.log(bulls);
+    // console.log(cows);
+    // console.log(temp);
+    return  bulls.length + "A" + cows.length + "B";
+};
+
+console.log(getHint("1807","7810"));
+
+console.log(getHint("1123","0111"))
+
--- rangeSumQuery.js
+++ rangeSumQuery.js
@@ -42,23 +42,6 @@ NumArray.prototype.sumRange = function(i, j) {
 var numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
 console.log(numArray.sumRange(2, 5));
 
-function NumArray1(nums) {
-  this.sums = [];
-  var sum = 0;
-  for (var i = 0; i < nums.length; i++) {
-    sum += nums[i];
-    this.sums.push(sum);
-  }
-  console.log(this.sums);
-}
-
-NumArray1.prototype.sumRange = function(i, j) {
-  return this.sums[j] - (i > 0 ? this.sums[i - 1] : 0);
-};
-
-var numArray1 = new NumArray1([-2, 0, 3, -5, 2, -1]);
-console.log(numArray1.sumRange(0, 5));
-
 /**
  * Your NumArray object will be instantiated and called as such:
  * var numArray = new NumArray(nums);
