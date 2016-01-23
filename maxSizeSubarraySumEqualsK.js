a253937e76351dfdbe8584ef53836736359cd039
Raymond Ho <chunkiat82@gmail.com>
2016-01-23 13:14:10 +0800

    maxSize array understood

--- maxSizeSubarraySumEqualsK.js
+++ maxSizeSubarraySumEqualsK.js
@@ -16,33 +16,82 @@
  * @param {number} k
  * @return {number}
  */
-var maxSubArrayLen = function(nums, k) {
+var maxSubArrayLenNSQ = function(nums, k) {
 
     var foundCount = 0;
 
+    var sum = [];
+    var total = 0;
+
     function countArray(nums, k) {
-        
-        var sum = 0;
+                
         var count = 0;  
-        nums.forEach(num => {
+        nums.forEach((num, index) => {
             count++;
-            sum += num;
-            if (sum === k){
-                foundCount = count;
+            if (index > 0){
+                if (sum[index] === undefined){                    
+                    sum[index] = sum[index-1] + num;                        
+                }else{
+                    sum[index] = sum[index-1] + num; 
+                }
+            }else{                
+                if (sum[index] === undefined){                    
+                    sum[index] = num;                    
+                }else{
+                    sum[index] = num;
+                }                      
+            }
+            
+
+            if (sum[index] === k){
+                if (foundCount < count){
+                    foundCount = count;    
+                }                
             }       
         });
         
     }
     while (nums.length > foundCount ){
-        countArray(nums, k);
+        countArray(nums, k);        
         nums.shift();
+
     }
-    
+    console.log('----------------------------------------');    
     return foundCount;
 };
 
+var maxSubArrayLen = function(nums, k) {
 
-console.log(maxSubArrayLen([1, -1, 5, -2, 3], 3));
+    if (nums === null || nums.length === 0)
+        return 0;
+
+    var n = nums.length;
+    for (var j = 1; j < n; j++){        
+        nums[j] += nums[j - 1];
+    }
+
+    var map = {};
+
+    map[0] = -1; // add this fake entry to make sum from 0 to j consistent
+
+    var max = 0;
+    for (var i = 0; i < n; i++) {
+        if (map[nums[i] - k] !== undefined){         
+            max = Math.max(max, i - map[(nums[i] - k)]);
+        }
+        if (map[nums[i]] === undefined) {
+            // keep only 1st duplicate as we want first index as left as possible
+            map[nums[i]]=i;
+        }
+    }
+
+    return max;
+    
+};
+
+
+
+//console.log(maxSubArrayLen([1, -1, 5, -2, 3], 3));
 
 console.log(maxSubArrayLen([-2, -1, 2, 1], 1));
 
