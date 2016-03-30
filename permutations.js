98b1b0b1ef20a8d66fae035299a028f5e080fcd8
Raymond Ho <chunkiat82@gmail.com>
2016-03-30 09:37:29 +0800

    a different one

---
+++ permutations.js
@@ -0,0 +1,35 @@
+function permutations(str){
+var arr = str.split(''),
+    len = arr.length, 
+    perms = [],
+    rest,
+    picked,
+    restPerms,
+    next;
+
+    if (len == 0)
+        return [str];
+
+    //console.log(str);
+
+    for (var i=0; i<len; i++)
+    {
+        rest = Object.create(arr);
+        picked = rest.splice(i, 1);
+
+        restPerms = permutations(rest.join(''));
+
+        console.log("picked",picked);
+        console.log("restPerms",restPerms);
+
+       for (var j=0, jLen = restPerms.length; j< jLen; j++)
+       {
+           next = picked.concat(restPerms[j]);
+           perms.push(next.join(''));
+       }
+    }
+   return perms;
+}
+permutations('abcd');
+
+// console.log(permutations('abcd'));
